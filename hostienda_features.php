f92a756fcc23e57b2aa712536f2c4881ddd518ae
Grimsley Hostienda <gj@hostienda.com>
2021-11-12 08:51:19 -0400

    Inicio del modulo

---
+++ hostienda_features.php
@@ -0,0 +1,64 @@
+<?php 
+	if(!defined('_PS_VERSION_')){
+		exit();
+	}
+
+	/**
+	 * 
+	 */
+	class Hostienda_features extends Module
+	{
+		
+		public function __construct()
+		{
+			
+			$this->name = 'hostienda_features';
+            $this->author = 'Grimsley Hostienda';
+            $this->tab = 'administration';
+            $this->version = '1.0.0';
+            $this->need_instance = false;
+            $this->bootstrap = true;
+            $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+
+            parent::__construct();
+
+            $this->displayName = $this->l('Grupo de Características');
+            $this->description = $this->l('Módulo para añadir las plantillas de características (WindCenter).');
+            $this->confirmUninstall = $this->l('¿Seguro que deseas desinstalar?');
+		}
+
+		public function install(){
+			if (Shop::isFeatureActive()) {
+	        	Shop::setContext(Shop::CONTEXT_ALL);
+	    	}
+
+	    	return(
+
+	    		parent::install()
+	    		&& $this->registerHook('displayAdminProductsMainStepRightColumnBottom')
+	    		&& $this->registerHook('displayBackOfficeHeader')
+	    		
+	    	);
+		}
+
+		public function hookDisplayAdminProductsMainStepRightColumnBottom() {
+	
+			return $this->display(__FILE__,'views/templates/hook/hostienda_features.tpl');
+		}
+
+		public function hookDisplayBackOfficeHeader(){
+
+			$this->context->controller->addCSS([
+	            $this->getPathUri() . 'views/css/hostienda_features.css',
+        	]);
+			$this->context->controller->addJS([
+	            $this->getPathUri() . 'views/js/hostienda_features.js',
+        	]);
+		}
+
+		public function uninstall(){
+			return(
+				parent::uninstall()
+			);
+		}
+	}
