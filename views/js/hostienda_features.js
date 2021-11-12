21a804afd7e0801616b82eeeb55a3911092ea997
Grimsley Hostienda <gj@hostienda.com>
2021-11-12 11:14:43 -0400

    gitflow-feature-stash: jquery-features

--- views/js/hostienda_features.js
+++ views/js/hostienda_features.js
@@ -1,5 +1,42 @@
 $(document).ready( function(){
 
-		console.log('hola')
+	let features = [
+
+		{
+			id : 1,
+			name: 'prueba',
+			values: [
+				1,
+				2,
+			],
+		},
+		{
+			id : 2,
+			name: 'fundas',
+			values: [
+				3,
+				4,
+				5,
+				6,
+			],
+		},
+
+	];
+
+	$.each(features, function(index,item){
+		$('#hostienda_features_select').append(new Option(item.name.toUpperCase(), item.id,));
+	});
+	$('#hostienda_features_select').on('change', function(){
+
+		feature = features.find(feature => feature.id == $(this).val());
+			if($('.row.product-feature').length>0){
+				$('.row.product-feature').remove();
+			}
+
+		$.each(feature.values, function(index, el) {
+			$('#add_feature_button').click();
+			$('.feature-selector.custom-select.select2-hidden-accessible').last().val(el).trigger('change');
+		});
+	});
 
 });
